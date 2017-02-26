d4a76d64eb7fd064f397284e0ac017b3449de7de
nzhul <dobromirivanov1@gmail.com>
2017-02-26 11:20:59 +0200

    Progress on calculator page

---
+++ js/calculator.js
@@ -0,0 +1,36 @@
+﻿// Initialize tooltips
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
+// Initialize
+
+$(document).ready(function () {
+	var allSliderContainers = $('.calculator-slider');
+
+	for (var i = 0; i < allSliderContainers.length; i++) {
+		var container = $(allSliderContainers[i]);
+		var slider = container.find('.slider-range-min');
+		var amountInput = container.find('input.amount');
+
+		var min = slider.data('min');
+		var max = slider.data('max');
+		var defaultValue = slider.data('default');
+
+		amountInput.val(defaultValue + "cm");
+
+		$(slider[0]).slider({
+			range: "min",
+			value: defaultValue,
+			animate: true,
+			min: min,
+			max: max,
+			slide: function (event, ui) {
+				$(event.target).parents('.calculator-slider').find('input.amount').val(ui.value + "cm");
+			}
+		});
+	}
+});
+
+
+// Calculation
