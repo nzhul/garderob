a1ec78fa93ec9a205b05457c8aa9fee24fc538ec
nzhul <dobromirivanov1@gmail.com>
2017-03-19 17:58:44 +0200

    working on the contact us page

--- App/App.Web/Controllers/HomeController.cs
+++ App/App.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using App.Web.Models;
+using System.Configuration;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace App.Web.Controllers
 {
@@ -15,7 +18,40 @@ namespace App.Web.Controllers
 
 		public ActionResult Contact()
 		{
-			return View();
+			var model = new ContactFormInputModel();
+			return View(model);
+		}
+
+		[HttpPost]
+		public ActionResult Contact(ContactFormInputModel contactData)
+		{
+			if (ModelState.IsValid)
+			{
+				string sender = ConfigurationManager.AppSettings["emailSender"];
+				string receiver = ConfigurationManager.AppSettings["emailReceiver"];
+
+				MailMessage mailMessage = new MailMessage(sender, receiver);
+				mailMessage.IsBodyHtml = true;
+				mailMessage.Subject = "Запитване (контактна форма): ";
+				mailMessage.Body = "Имена: " + contactData.Name + "<br/>" +
+								   "Email: " + contactData.Email + "<br/>" +
+								   "Телефон: " + contactData.Phone + "<br/><br/>" +
+								   "Запитване: <br/>" + contactData.Content;
+
+				SmtpClient smtpClient = new SmtpClient();
+
+				// The settings are in web.config file
+				smtpClient.Send(mailMessage);
+
+				return Content(@"<div class='alert alert-dismissable alert-success'>
+                            <button type='button' class='close' data-dismiss='alert'>×</button>
+                            <strong>Съобщението беше изпратено успешно!</strong>
+                        </div>");
+			}
+			else
+			{
+				return View("Contact", contactData);
+			}
 		}
 	}
 }
