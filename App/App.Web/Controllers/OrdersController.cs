cfcd4b24296246de710eb7b18b9cef6777a70a7f
nzhul <dobromirivanov1@gmail.com>
2017-03-21 22:17:32 +0200

    working on static pages

---
+++ App/App.Web/Controllers/OrdersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Web.Controllers
+{
+	public class OrdersController : Controller
+	{
+		public ActionResult Make()
+		{
+			return this.View();
+		}
+	}
+}
