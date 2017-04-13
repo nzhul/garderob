76ceec95d4beefe881b6f1f54e684e93cb565eff
nzhul <dobromirivanov1@gmail.com>
2017-04-13 23:51:21 +0300

    starting work on materials crud

--- App/App.Web/Areas/Administration/Controllers/MaterialsController.cs
+++ App/App.Web/Areas/Administration/Controllers/MaterialsController.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using App.Data.Service.Abstraction;
+using App.Models.Materials;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web.Mvc;
 
 namespace App.Web.Areas.Administration.Controllers
 {
 	public class MaterialsController : BaseController
 	{
+		private IMaterialsService materialsService;
+
+		public MaterialsController(IMaterialsService materialsService)
+		{
+			this.materialsService = materialsService;
+		}
+
+		[HttpGet]
+		public ActionResult Index()
+		{
+			IEnumerable<MaterialCategory> model = this.materialsService.GetAllCategoriesWithMaterials().ToList();
+
+			return this.View(model);
+		}
 	}
 }
