685946f3f0ca7a6eb2f437216104cf6c95d6f634
nzhul <dobromirivanov1@gmail.com>
2017-04-18 23:29:17 +0300

    adding materials list page

---
+++ App/App.Web/Controllers/MaterialsController.cs
@@ -0,0 +1,29 @@
+﻿using App.Data.Service.Abstraction;
+using App.Models.Materials;
+using System.Web.Mvc;
+
+namespace App.Web.Controllers
+{
+	public class MaterialsController : Controller
+	{
+		private IMaterialsService materialsService;
+
+		public MaterialsController(IMaterialsService materialsService)
+		{
+			this.materialsService = materialsService;
+		}
+
+		[HttpGet]
+		public ActionResult List(int id)
+		{
+			MaterialCategory model = this.materialsService.GetMaterialCategory(id, false, false);
+
+			if (model != null)
+			{
+				return this.View(model);
+			}
+
+			return this.HttpNotFound();
+		}
+	}
+}
