4cc19545d36aaeddd6501110e77263002bc40a6f
nzhul <dobromirivanov1@gmail.com>
2018-03-16 21:04:16 +0200

    adding alertify for administration and fixing image file extensions when downloading

--- App/App.Web/Controllers/ImageController.cs
+++ App/App.Web/Controllers/ImageController.cs
@@ -1,49 +1,56 @@
 ﻿using App.Data.Service.Abstraction;
 using App.Models.Images;
+using System.Net.Mime;
 using System.Web.Mvc;
 
 namespace App.Web.Controllers
 {
-	public class ImageController : Controller
-	{
-		IImagesService imagesService;
+    public class ImageController : Controller
+    {
+        IImagesService imagesService;
 
-		public ImageController(IImagesService imagesService)
-		{
-			this.imagesService = imagesService;
-		}
+        public ImageController(IImagesService imagesService)
+        {
+            this.imagesService = imagesService;
+        }
 
-		public ActionResult Show(int id, string imageType)
-		{
-			Image dbImage = this.imagesService.GetImage(id);
+        public ActionResult Show(int id, string imageType)
+        {
+            Image dbImage = this.imagesService.GetImage(id);
 
-			if (dbImage != null)
-			{
-				byte[] imageData = null;
+            if (dbImage != null)
+            {
+                byte[] imageData = null;
 
-				switch (imageType)
-				{
-					case "small":
-						imageData = dbImage.Small;
-						break;
-					case "medium":
-						imageData = dbImage.Medium;
-						break;
-					case "big":
-						imageData = dbImage.Big;
-						break;
-					default:
-						imageData = dbImage.Small;
-						break;
-				}
+                switch (imageType)
+                {
+                    case "small":
+                        imageData = dbImage.Small;
+                        break;
+                    case "medium":
+                        imageData = dbImage.Medium;
+                        break;
+                    case "big":
+                        imageData = dbImage.Big;
+                        break;
+                    default:
+                        imageData = dbImage.Small;
+                        break;
+                }
 
-				//TODO: return png by default or by setting
-				return File(imageData, "image/jpg");
-			}
-			else
-			{
-				return HttpNotFound();
-			}
-		}
-	}
+                ContentDisposition cd = new ContentDisposition
+                {
+                    FileName = dbImage.Id + ".jpg",
+                    Inline = true
+                };
+
+                Response.AppendHeader("Content-Disposition", cd.ToString());
+                return File(imageData, "image/jpg");
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
 }
